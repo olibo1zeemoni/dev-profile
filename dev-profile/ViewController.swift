c984ca126045fd4110871d1d993e20aee4bb081d
Zeemoni <gaboahene1@gmail.com>
2021-10-15 16:43:16 +0000

    Initial Commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Olibo moni on 15/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
